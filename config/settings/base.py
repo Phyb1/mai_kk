050b7aed9a0a370356d370e2b18e3bbf5f8d42d7
Phyb1 <phibeon@yahoo.com>
2026-08-05 15:54:55 +0200

    static issue

--- config/settings/base.py
+++ config/settings/base.py
@@ -73,7 +73,7 @@ TIME_ZONE = "Africa/Harare"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/mai_kk/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
