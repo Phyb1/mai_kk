b366771c362b519e40a37dd39d2df9745393523a
Phyb1 <phibeon@yahoo.com>
2026-08-05 11:46:38 +0200

    static fix drpin static und use re_pa

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve as static_serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,5 +10,16 @@ urlpatterns = [
     path("", include("apps.core.urls")),
 ]
 
+# Serve static and media explicitly for Passenger sub-URI deployments
+# This works even when STATIC_URL = '/onspot/static/' because Django matches
+# against PATH_INFO after SCRIPT_NAME is stripped by Passenger
+urlpatterns += [
+    re_path(r"^static/(?P<path>.*)$", static_serve, {"document_root": settings.STATIC_ROOT}),
+    re_path(r"^media/(?P<path>.*)$", static_serve, {"document_root": settings.MEDIA_ROOT}),
+]
+
+# Keep this for DEBUG if you also want runserver to work with /media/
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", static_serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
